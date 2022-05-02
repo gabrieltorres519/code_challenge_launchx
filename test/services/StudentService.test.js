4c7652182aff89f4277e762a2f39c0a27ac30b7e
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-02 12:58:00 -0500

    Modelado requeriiento y agregada  primemr prueba de unidad para la clase StudentService

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,9 +1,12 @@
-describe('Unit tests for StudentService Class',()=>{
-    test('1) Try',()=>{
-        //Código de la app
-            let a = 2;
+const StudentService = require("../../lib/services/StudentService");
 
+describe('Unit tests for StudentService Class',()=>{
+    test('1) Return all the students in the Json file as a service mediator',()=>{
+        //Código de la app
+            const Students = StudentService.showAllStudents("visualpartners.json");
+            
         //Validaciones
-        expect(a).toBe(2)
+        expect(Students).not.toBeUndefined()
+        expect(Students[0]).not.toBeUndefined()
     })
 })
