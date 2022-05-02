e89df01cee51170c1fc6b749d234e09e0d006fe0
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-02 12:35:40 -0500

    Pasada la primer pruba de unidad de la clase Reader para traer los datos del archivo Json

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -4,7 +4,7 @@ describe('Unit tests for Reader Class',()=>{
     
     test('1) Functionality to read the Json "Database" of students and obtain them all',()=>{
         //Código de la app
-        const Students = Reader.readDBFile("visualpartners.json");
+        const Students = Reader.readJsonFile("visualpartners.json");
         
         //Validaciones
         expect(Students).not.toBeUndefined()
