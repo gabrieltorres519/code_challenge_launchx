c505304c0226f9e27fce8148e9218fb03abb25fb
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-02 12:03:49 -0500

    Agregando primeras pruebas de unidad por default

---
+++ test/controllers/StudentController.test.js
@@ -0,0 +1,9 @@
+describe('Unit tests for StudentController Class',()=>{
+    test('1) Try',()=>{
+        //Código de la app
+            let a = 2;
+
+        //Validaciones
+        expect(a).toBe(2)
+    })
+})
---
+++ test/utils/Reader.test.js
@@ -0,0 +1,9 @@
+describe('Unit tests for Reader Class',()=>{
+    test('1) ',()=>{
+        //Código de la app
+            let a = 2;
+
+        //Validaciones
+        expect(a).toBe(2)
+    })
+})
